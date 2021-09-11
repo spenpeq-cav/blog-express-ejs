c6e194f669ecf06aaa79e89da129954a5b02351d
Spencer Pequegnat <spencer.peq@gmail.com>
2021-09-11 12:33:32 -0700

    Preparing for heroku deployment

--- app.js
+++ app.js
@@ -96,7 +96,11 @@ app.post("/signup", function(req, res){
   console.log("Sign Up Triggered")
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started...");
 });
